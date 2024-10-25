7031160c95bacd0e0ceb4e347d4218316acf1847
rgutierrez27 <nitro.frz@gmail.com>
2024-10-24 20:18:08 -0500

    envio de credenciales2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('qr:generar-masivo')->hourly();
-        $schedule->command('qr:generar-masivo')->hourly();
+        $schedule->command('virtual:envio-enlace')->hourly();
     }
 
     /**
